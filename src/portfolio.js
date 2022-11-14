05347d377b3ac08fe56278d6c7a3e2888857a65f
Edward Carey <edward.j.carey@stonybrook.edu>
2022-11-14 13:49:08 -0500

    pages changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io',
+  homepage: 'https://ed-joe.github.io',
   title: 'About Me',
 }
 
